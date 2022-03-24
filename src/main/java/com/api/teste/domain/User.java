8a02cffe43dd21ca5865ab1c46471aa57db00fc8
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-03-24 10:01:37 -0300

    Classe de usuario

---
+++ src/main/java/com/api/teste/domain/User.java
@@ -0,0 +1,30 @@
+package com.api.teste.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String password;
+}
