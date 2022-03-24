89a3f8663d04a99dc6e383cbc3623438d0fc2afd
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-03-24 09:36:50 -0300

    Projeto iniciado

---
+++ src/main/java/com/api/teste/ApiTesteUnitariosApplication.java
@@ -0,0 +1,13 @@
+package com.api.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTesteUnitariosApplication.class, args);
+	}
+
+}
