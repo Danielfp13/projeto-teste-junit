4c1ee167cffcbd6842f6d63c85413ebcac134ba9
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-03-25 07:54:52 -0300

    Teste para a classe de UserResource no metodo findById sucesso

--- src/main/java/com/api/teste/services/excptions/DataIntegrityViolationException.java
+++ src/main/java/com/api/teste/services/excptions/DataIntegrityViolationException.java
@@ -3,7 +3,11 @@ package com.api.teste.services.excptions;
 public class DataIntegrityViolationException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	public DataIntegrityViolationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
 	public DataIntegrityViolationException(String message) {
 		super(message);
 	}
